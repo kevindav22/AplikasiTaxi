d43dac3268185595469fc43ac7129c7311b572b6
kepin.pery@gmail.com <kepin.pery@gmail.com>
2022-03-25 20:05:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //Pengesehan nilai properties
+            taxi.DriverName = "jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //Pengambilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty);
+            Console.WriteLine("Number Off Passenger : {0}", NumPassenger);
+
+
+            
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\nJono Sedang Menjemput Penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Jono Selesai Menjemput Penumpang");
+
+            Console.ReadKey();
+        }
+   
+
+    }
+}
